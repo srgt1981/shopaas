e711ebfbe17819bf4fa1fd97a59c850417361000
jose.m.beas <jose.m.beas@31f8d03e-b85c-11dd-b55b-2da8d274f894>
2009-02-05 07:23:10 +0000

    Servicios y los tests de aceptación (sólo demo por ahora)

---
+++ shopaas/services-acceptance-test/src/test/java/shopaas/concordion/DemoTest.java
@@ -0,0 +1,12 @@
+package shopaas.concordion;
+
+import org.concordion.integration.junit4.ConcordionRunner;
+import org.junit.runner.RunWith;
+
+@RunWith(ConcordionRunner.class)
+public class DemoTest {
+    
+    public String greetingFor(String firstName) {
+        return String.format("Hello %s!", firstName);
+    }
+}
